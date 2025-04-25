e3c4fa587c105f356bc78c55b0b93dc2304b8b8e
Adrià Martinez <adria.martinez@immfly.com>
2025-04-25 11:29:30 +0200

    Project setup

---
+++ src/main/java/com/amf/pfm/transaction/PfmTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.amf.pfm.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfmTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfmTransactionServiceApplication.class, args);
+	}
+
+}
